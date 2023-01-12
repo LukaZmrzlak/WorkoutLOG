622e6702215e5662edb19ea5542cd787a760eed5
LukaZmrzlak <zmrzlak.luka@gmail.com>
2023-01-12 13:13:17 +0100

    Adding start of login and register activities

---
+++ MobileApp/app/src/main/java/si/uni_lj/fe/workoutlog/RegisterActivity.java
@@ -0,0 +1,14 @@
+package si.uni_lj.fe.workoutlog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
