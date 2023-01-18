952dd7118e8252c4ebbaee9268904d96c5a27470
LukaZmrzlak <zmrzlak.luka@gmail.com>
2023-01-18 17:31:11 +0100

    Adding almost finished logging activities UI

--- MobileApp/app/src/main/java/si/uni_lj/fe/workoutlog/FitnessActivity.java
+++ MobileApp/app/src/main/java/si/uni_lj/fe/workoutlog/FitnessActivity.java
@@ -2,17 +2,13 @@ package si.uni_lj.fe.workoutlog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class SettingsActivity extends AppCompatActivity {
+public class FitnessActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-
+        setContentView(R.layout.activity_fitness);
     }
 }
--- MobileApp/app/src/main/java/si/uni_lj/fe/workoutlog/HomeActivity.java
+++ MobileApp/app/src/main/java/si/uni_lj/fe/workoutlog/HomeActivity.java
@@ -2,7 +2,11 @@ package si.uni_lj.fe.workoutlog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +14,58 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        configureLogoutImageButton();
+        configureCardioButton();
+        configureFitnessButton();
+        configureSettingsImageButton();
+    }
+
+    private void configureFitnessButton()
+    {
+        Button FitnessButton = (Button) findViewById(R.id.fitness);
+        FitnessButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(HomeActivity.this, FitnessActivity.class));
+            }
+        });
+    }
+
+    private void configureCardioButton()
+    {
+        Button CardioButton = (Button) findViewById(R.id.cardio);
+        CardioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(HomeActivity.this, CardioActivity.class));
+            }
+        });
+    }
+
+    private void configureSettingsImageButton()
+    {
+        ImageButton SettingsButton = (ImageButton) findViewById(R.id.settings);
+        SettingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(HomeActivity.this, SettingsActivity.class));
+            }
+        });
+    }
+
+    private void configureLogoutImageButton()
+    {
+        ImageButton LogoutButton = (ImageButton) findViewById(R.id.logout);
+        LogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
